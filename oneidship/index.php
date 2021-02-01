3270d986eb524afc88870e97007daeaea77a3bf1
rathod4rathod <rathod4rathod@gmail.com>
2021-02-01 12:01:04 +0530

    oneidnet

---
+++ oneidship/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Submit Claim!</title>
+         <script src="js/classie.js"></script>
+        <script src="js/modalEffects.js"></script>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="images/favicon.png" type="image/png">
+        <link rel="stylesheet" href="css/clickmenu.css" type="text/css">
+        <link rel="stylesheet" href="css/component.css" type="text/css">
+    </head>
+    <body>
+        <div class="oisdes_module_bgmain_wrapper"><!--image wrapper background start here-->
+            <div class="comingsoon_bgwrap">
+                <div class="comingsoon_bgcolor">
+                    <div class="wi50">
+                        <div class="oisdes_logooisdes">
+                            <a href="#"><img alt="logo" src="images/oislogo.png"></a>
+                        </div>
+                        <h1 class="acenter normal"> Module Temporarily   </h1> 
+                        <h1 class="acenter normal">  Deactivated  </h1> 
+                    </div>
+                    <div class="wi70">
+                        <div class="wi60">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="md-overlay"></div><!-- the overlay element -->
+       
+        <script>
+            // this is important for IEs
+            var polyfilter_scriptpath = '/js/';
+        </script>
+    </body>
+</html>
---
+++ oneidship/js/classie.js
@@ -0,0 +1,107 @@
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
+if(window.location.hostname=="127.0.0.1" || window.location.hostname=="localhost" || window.location.hostname=="192.168.0.100"){
+var oneidship_url = document.location.origin+"/oneidship/";    
+var onenetwork_url = document.location.origin+"/onenetwork/";    
+var oneidnet_url = document.location.origin+"/oneidnet/";    
+}else{
+    var oneidship_url = document.location.origin;
+    var onenetwork_url = "https://onenetwork.oneidnet.com/";    
+    var oneidnet_url = "https://www.oneidnet.com/";    
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+            c = c.substring(1);
+        if (c.indexOf(name) == 0)
+            return c.substring(name.length, c.length);
+    }
+    return "";
+}
+var seg=getCookie("oud");
+if(self == top || seg == ""){
+    top.location.href=oneidnet_url+"redirection?url="+oneidship_url;
+}
